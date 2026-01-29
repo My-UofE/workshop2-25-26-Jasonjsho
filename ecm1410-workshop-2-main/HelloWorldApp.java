860c11905336c1b3c71970dcabc32325bbd170b3
Jasonjsho <jh1827@exeter.ac.uk>
2026-01-29 19:03:44 +0000

    task 1 done

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -2,7 +2,6 @@
 // HelloWorld application
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! My name is " + args[0] + ". I am " + args[1] + " years old.");
     }
 }
-
